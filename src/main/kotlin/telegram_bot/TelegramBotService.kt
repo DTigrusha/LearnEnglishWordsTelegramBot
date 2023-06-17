341796e449ebd7ba64f54b5fa621d62d3cc8425b
DTigrusha <DTigrusha@users.noreply.github.com>
2023-06-17 13:10:03 +0300

    TB-step_8 create const val

--- src/main/kotlin/telegram_bot/TelegramBotService.kt
+++ src/main/kotlin/telegram_bot/TelegramBotService.kt
@@ -7,8 +7,12 @@ import java.net.http.HttpResponse
 
 class TelegramBotService(val botToken: String) {
 
+    companion object {
+        const val BOT_URL = "https://api.telegram.org/bot"
+    }
+
     fun getUpdates(botToken: String, updateId: Int): String {
-        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+        val urlGetUpdates = "$BOT_URL$botToken/getUpdates?offset=$updateId"
         val client: HttpClient = HttpClient.newBuilder().build()
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
@@ -16,7 +20,7 @@ class TelegramBotService(val botToken: String) {
     }
 
     fun sendMessage(botToken: String, chatId: Int, messageText: String): String {
-        val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$messageText"
+        val urlSendMessage = "$BOT_URL$botToken/sendMessage?chat_id=$chatId&text=$messageText"
         val client: HttpClient = HttpClient.newBuilder().build()
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendMessage)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
@@ -24,4 +28,3 @@ class TelegramBotService(val botToken: String) {
     }
 
 }
-
